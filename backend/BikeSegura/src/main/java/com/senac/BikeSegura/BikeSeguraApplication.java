0c76862cb10a1eb3e597bdf9c984e168b67d3d28
Matheus Muniz <matheusmuniz0810@gmail.com>
2025-10-20 17:19:51 -0300

    feat: Criar estrutura base do projeto(backend)

---
+++ backend/BikeSegura/src/main/java/com/senac/BikeSegura/BikeSeguraApplication.java
@@ -0,0 +1,13 @@
+package com.senac.BikeSegura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeSeguraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeSeguraApplication.class, args);
+	}
+
+}
---
+++ backend/BikeSegura/src/test/java/com/senac/BikeSegura/BikeSeguraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.BikeSegura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeSeguraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
